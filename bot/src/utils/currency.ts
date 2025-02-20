05e59a925b6377cd6eb31098e5a0384813675968
Pegoku <pegoku08@gmail.com>
2025-02-20 21:37:14 +0100

    add types to currency tool

--- bot/src/utils/currency.ts
+++ bot/src/utils/currency.ts
@@ -1,53 +1,59 @@
-import axios from 'axios';
+import axios from "axios";
 
 import { config } from "dotenv-vault";
-import fs from 'fs';
+import fs from "fs";
 config();
 
-const {
-	EXCHANGE_API_KEY
-} = process.env;
+const { EXCHANGE_API_KEY } = process.env;
 
 if (!EXCHANGE_API_KEY) {
-    throw new Error('Missing EXCHANGE_API_KEY');
+  throw new Error("Missing EXCHANGE_API_KEY");
 }
 
 interface Currency {
-    conversion_rates: { [key: string]: number };
+  conversion_rates: { [key: string]: number };
 }
-
 
 const API_URL = `https://v6.exchangerate-api.com/v6/${EXCHANGE_API_KEY}/latest/USD`;
 
 export async function getCurrencies(): Promise<any> {
-    try{
+  try {
     const response = await axios.get(API_URL);
     return response.data;
-    } catch (error) {
-        console.error('Error fetching currency data:', error);
-        throw error;
-    }
-
+  } catch (error) {
+    console.error("Error fetching currency data:", error);
+    throw error;
+  }
 }
 
-let currenciesRequest: any;
+let currenciesRequest: Currency;
 
-getCurrencies().then(data => {
+getCurrencies()
+  .then((data) => {
     currenciesRequest = data;
     console.log(currenciesRequest);
 
     const rates = currenciesRequest.conversion_rates;
     console.log(rates);
-    console.log(rates['USD']);
-    fs.writeFile('src/utils/currency.json', JSON.stringify(rates, null, 2), (err) => {
-        if (err) {
-            console.error('Error writing to currency.json:', err);
-        } else {
-            console.log('Currency rates successfully written to currency.json');
+    console.log(rates["USD"]);
+    if (rates["USD"] == 1) {
+      fs.writeFile(
+        "src/utils/currency.json",
+        JSON.stringify(rates, null, 2),
+        (err) => {
+          if (err) {
+            console.error("Error writing to currency.json:", err);
+          } else {
+            console.log("Currency rates successfully written to currency.json");
+          }
         }
-    });
-}).catch(error => {
+      );
+    } else {
+      console.error("Error fetching data: USD is not 1"); // Just to be safe
+    }
+  })
+  .catch((error) => {
     console.error(error);
-});
+  });
 
 // console.log(API_URL);
