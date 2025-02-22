5fdc701c114e5ae9deb42f8f354249ad577d2203
vic <74506415+v1ctorio@users.noreply.github.com>
2025-02-22 19:17:38 +0100

    Update spain info page and remove useless element

--- website/src/app/layout.config.tsx
+++ website/src/app/layout.config.tsx
@@ -15,7 +15,6 @@ export const baseOptions: BaseLayoutProps = {
           src="https://cloud-2lknei65b-hack-club-bot.vercel.app/0johnny.jpg"
           style={{ height: 32, width: "auto" }}
         />
-        <circle cx={12} cy={12} r={12} fill="currentColor" />
         Johnny customs
       </>
     ),
