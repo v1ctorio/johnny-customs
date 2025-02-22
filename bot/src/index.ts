9330dd8b90b6494f6782d55fa82cd97d5b9db947
Pegoku <pegoku08@gmail.com>
2025-02-22 23:58:02 +0100

    modified bot

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -5,7 +5,16 @@ import { config } from "dotenv";
 import fs from "fs";
 import path from "path";
 import _isoModalOptions from "./utils/isoModalOptions.json" assert { type: "json" };
+import _iso2CountryCodes from "./utils/iso2CountryCodes.json" assert { type: "json" };
+import { createTypeReferenceDirectiveResolutionCache } from "typescript";
+import { lookup } from "dns";
 config();
+
+interface CountryCodes {
+  [key: string]: string;
+}
+
+const iso2CountryCodes: CountryCodes = _iso2CountryCodes;
 
 const {
   SLACK_BOT_TOKEN,
@@ -34,111 +43,93 @@ slack.command(
       console.log(body);
       console.log(body.trigger_id);
 
-
-			const result = await client.views.open({
-				// Pass a valid trigger_id within 3 seconds of receiving it
-				trigger_id: body.trigger_id,
-				// View payload
-				view: {
-					type: "modal",
-					// View identifier
-					callback_id: "view_1",
-					title: {
-						type: "plain_text",
-						text: "Modal title",
-					},
-					blocks: [
-						{
-							type: "section",
-							text: {
-								type: "mrkdwn",
-								text: "Welcome to a modal with _blocks_",
-							},
-							accessory: {
-								type: "button",
-								text: {
-									type: "plain_text",
-									text: "Click me!",
-								},
-								action_id: "button_abc",
-							},
-						},
-						{
-							type: "input",
-							block_id: "item_input",
-							label: {
-								type: "plain_text",
-								text: "What did you pay customs for?",
-							},
-							element: {
-								type: "plain_text_input",
-								action_id: "dreamy_input",
-								multiline: false,
-							},
-						},
-						{
-							type: "input",
-							block_id: "declared_input",
-							label: {
-								type: "plain_text",
-								text: "What was the declared value?",
-							},
-							element: {
-								type: "plain_text_input",
-								action_id: "dreamy_input",
-							},
-						},
-						{
-							type: "input",
-							block_id: "paid_input",
-							label: {
-								type: "plain_text",
-								text: "Paid customs",
-							},
-							element: {
-								type: "plain_text_input",
-								action_id: "dreamy_input",
-							},
-						},
-						{
-							type: "input",
-							block_id: "iso_select_input",
-							label: {
-								type: "plain_text",
-								text: "Select an option",
-							},
-							element: {
-								type: "static_select",
-								action_id: "iso_select_action",
-								options: _isoModalOptions.slice(0, 100).map(option => ({ // Slice 100 bc it's slack max
-									text: {
-										type: "plain_text",
-										text: option.text.text,
-									},
-									value: option.value,
-								})),
-							},
-						},
-						{
-							type: "input",
-							block_id: "notes_input",
-							label: {
-								type: "plain_text",
-								text: "Additional information",
-							},
-							element: {
-								type: "plain_text_input",
-								action_id: "dreamy_input",
-								multiline: true,
-							},
-						},
-					],
-					submit: {
-						type: "plain_text",
-						text: "Submit",
-					},
-				},
-			});
+      const result = await client.views.open({
+        // Pass a valid trigger_id within 3 seconds of receiving it
+        trigger_id: body.trigger_id,
+        // View payload
+        view: {
+          type: "modal",
+          // View identifier
+          callback_id: "view_1",
+          title: {
+            type: "plain_text",
+            text: "Customs Submission",
+          },
+          blocks: [
+            {
+              type: "input",
+              block_id: "item_input",
+              label: {
+                type: "plain_text",
+                text: "What did you pay customs for?",
+              },
+              element: {
+                type: "plain_text_input",
+                action_id: "dreamy_input",
+                multiline: false,
+              },
+            },
+            {
+              type: "input",
+              block_id: "declared_input",
+              label: {
+                type: "plain_text",
+                text: "What was the declared value? (in your local currency)",
+              },
+              element: {
+                type: "plain_text_input",
+                action_id: "dreamy_input",
+              },
+            },
+            {
+              type: "input",
+              block_id: "paid_input",
+              label: {
+                type: "plain_text",
+                text: "Paid customs (in your local currency)",
+              },
+              element: {
+                type: "plain_text_input",
+                action_id: "dreamy_input",
+              },
+            },
+            {
+              type: "input",
+              block_id: "iso_input",
+              label: {
+                type: "plain_text",
+                text: "ISO2 country code (e.g. US, IN, CA) ",
+              },
+              hint: {
+                type: "plain_text",
+                text: "Refer to https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Officially_assigned_code_elements for the list of codes.",
+              },
+              element: {
+                type: "plain_text_input",
+                action_id: "dreamy_input",
+              },
+            },
+            {
+              type: "input",
+              block_id: "notes_input",
+              label: {
+                type: "plain_text",
+                text: "Additional information",
+              },
+              element: {
+                type: "plain_text_input",
+                action_id: "dreamy_input",
+                multiline: true,
+              },
+              optional: true,
+            },
+          ],
+          submit: {
+            type: "plain_text",
+            text: "Submit",
+          },
+        },
+      });
       logger.info(result);
     } catch (error) {
       logger.error(error);
@@ -146,13 +137,32 @@ slack.command(
   }
 );
 
-slack.view("view_1", async ({ ack, body, view, logger }) => {
-	await ack();
-	console.log("View submitted");
-	console.log(view);
+slack.view("view_1", async ({ ack, body, view, logger, client }) => {
+  await ack();
+  console.log("View submitted");
+
+  const item = view.state.values.item_input.dreamy_input.value ?? "";
+  const declared = view.state.values.declared_input.dreamy_input.value ?? "";
+  const paid = view.state.values.paid_input.dreamy_input.value ?? "";
+  const iso: string = view.state.values.iso_input.dreamy_input.value ?? "";
+  const notes = view.state.values.notes_input.dreamy_input.value ?? "";
+
+
+  const country = iso
+    ? iso2CountryCodes[iso] || "Unknown country"
+    : "Unknown country";
+
+  logger.info("View submitted");
+  logger.info(`Item: ${item}`);
+  logger.info(`Declared Value: ${declared}`);
+  logger.info(`Paid Customs: ${paid}`);
+  logger.info(`ISO2 Country Code: ${iso}`);
+  logger.info(`Country: ${country}`);
+  logger.info(`Notes: ${notes}`);
 });
 
 (async () => {
   await slack.start();
   console.log("Slack bot is running");
 })();
+
