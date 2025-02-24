65d59fdca0fb6fc1674f2376c13ad73ec18d5f8d
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2025-02-24 22:54:28 +0100

    style: format some coe

--- bot/src/utils/dbToJSON.ts
+++ bot/src/utils/dbToJSON.ts
@@ -1,6 +1,6 @@
 import prisma from "db";
-import fs from "fs";
-import path from "path";
+import fs from "node:fs";
+import path from "node:path";
 
 const submissions = await prisma.chargeSubmission.findMany();
 
