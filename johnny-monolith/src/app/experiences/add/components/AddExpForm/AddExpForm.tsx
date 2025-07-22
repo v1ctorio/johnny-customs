ea3044f11a2ef7ebede8de472a688a62697a792d
Vic <vicxd@pm.me>
2025-07-22 19:17:27 +0200

    Updating mantine (please dont destroy everything)

--- johnny-monolith/src/app/experiences/add/components/AddExpForm/AddExpForm.tsx
+++ johnny-monolith/src/app/experiences/add/components/AddExpForm/AddExpForm.tsx
@@ -2,13 +2,28 @@
 
 import { SlackUserButton } from "@/app/experiences/components/SlackUserButton/SlackUserButton";
 import { submissionsTable, thingsTable } from "@/db/schema";
-import { Box, Flex, Input, InputWrapper, NativeSelect, Stack, Textarea, useMantineTheme } from "@mantine/core";
+import { Autocomplete, Box, Flex, Input, InputWrapper, NativeSelect, Select, Stack, Text, Textarea, UnstyledButton, useMantineTheme } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { useEffect, useState } from "react";
 
 export function AddExpForm({submitterID}:{submitterID:string}) {
+	type submitData = {
+    payment_date: string;
+    submitter: string;
+    country: string;
+    declared_value: number;
+    paid_customs: number;
+    thing_id: string;
+    submission_time?: Date | undefined;
+    approved?: boolean | undefined;
+    notes?: string | null | undefined;
+}
+
 	type submitData = typeof submissionsTable.$inferInsert
 	const [thingsList,setThingsList] = useState<{label:string,value:string}[]>([])
+
+	const [submitNewItem, setSubmitNewItem] = useState(false)
+
 	const theme = useMantineTheme()
 	useEffect(()=>{
 		fetch('/api/things')
@@ -27,7 +42,8 @@ export function AddExpForm({submitterID}:{submitterID:string}) {
 					<Stack w={800}>
 
 			<form>
-				<NativeSelect label="Thing" withAsterisk description="Item you paid customs for." data={thingsList} key={form.key('thing_id')}/>
+				<Select clearable auto label="Thing" withAsterisk description="Item you paid customs for." data={thingsList} key={form.key('thing_id')}/>
+				{!submitNewItem && <Text size="xs">The item you recived is not in the list? <Text span c="blue" style={{"cursor":"pointer"}} onClick={()=> setSubmitNewItem(true) }>Click here to add a new one</Text>.</Text>}
 				<Textarea label="Notes" description="Additional notes. (optional)" key={form.key('notes')}/>
 				<InputWrapper label="Submitting as">
 				<Box p='sm'>
